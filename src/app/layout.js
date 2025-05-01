f1d924982d898e694664594fa100bbda24cbd579
Kidus1921 <143596069+Kidus1921@users.noreply.github.com>
2025-05-01 17:55:55 +0300

    Update layout with preview Image

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,27 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Kidus Belete',
   description: 'Created by Kidus Belete',
+  openGraph: {
+    title: 'Kidus Belete',
+    description: 'Portfolio website of Kidus Belete – Developer & Designer.',
+    url: 'https://kidusbelete.onrender.com/', 
+    siteName: 'Kidus Belete',
+    images: [
+      {
+        url: 'https://ucarecdn.com/e33a0e45-cfd3-4ae1-a7c7-b5cd03d09386/previewImage.jpeg', 
+        width: 1200,
+        height: 630,
+        alt: 'Kidus Belete Portfolio Preview',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Kidus Belete',
+    description: 'Explore Kidus Belete\'s portfolio website.',
+    images: ['https://ucarecdn.com/e33a0e45-cfd3-4ae1-a7c7-b5cd03d09386/previewImage.jpeg'],
+  },
 }
 
 export default function RootLayout({ children }) {
